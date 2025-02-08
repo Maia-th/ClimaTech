da4b684a87c976f9dfdf6fa80d7628d583dd1ad6
Maia-Th <thiagomaia633@gmail.com>
2025-02-08 10:08:24 -0300

    update api

--- src/api/utils/jwt.js
+++ src/api/utils/jwt.js
@@ -1,7 +1,9 @@
 const jwt = require('jsonwebtoken');
+require('dotenv').config();
 
 const verifyToken = (req, res, next) => {
-  const token = req.headers['authorization'];
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
 
   if (!token) {
     return res.status(403).json({ message: 'Token não fornecido' });
@@ -13,7 +15,7 @@ const verifyToken = (req, res, next) => {
     req.userAccess = decoded.access;
     next();
   } catch (error) {
-    return res.status(401).json({ message: 'Token inválido' });
+    return res.status(401).json({ message: 'Token inválido!' });
   }
 };
 
