97298d482a1ad43b75340913ac31dd3b9caa2de2
JulianaElen <julianaesoliveira@gmail.com>
2025-02-08 19:21:52 -0300

    Ajuste rota

--- src/api/middleware/privateRoutes.js
+++ src/api/middleware/privateRoutes.js
@@ -3,8 +3,8 @@ const verifyToken = require('../utils/jwt');
 
 const privateRoutes = (app) => {
   // Rotas protegidas
-  app.get('/src/pages/config-1.html', verifyToken, (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'src', 'pages', 'config-1.html'));
+  app.get('/src/pages/config.html', verifyToken, (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'src', 'pages', 'config.html'));
   });
 
   app.get('/src/pages/dashboard.html', verifyToken, (req, res) => {
